b669b0060c1dac0c482f43575c4c725a932e83bf
KSteinke <kacpersteinkesb@gmail.com>
2024-02-08 19:45:35 +0100

    Init Changes

--- CarPrice_Server/Data/CarPriceDbContext.cs
+++ CarPrice_Server/Data/CarPriceDbContext.cs
@@ -1,8 +1,7 @@
 using CarPrice_Server.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace CarPrice_Server.Data
-{
+namespace CarPrice_Server.Data;
     public class CarPriceDbContext:DbContext
     {
         public CarPriceDbContext(DbContextOptions<CarPriceDbContext> options):base(options) 
@@ -15,6 +14,3 @@ namespace CarPrice_Server.Data
         public DbSet<Engine> Engines {get; set;}
         public DbSet<Voivoidship> Voivoidships {get; set;}
     }
-
-    
-}
