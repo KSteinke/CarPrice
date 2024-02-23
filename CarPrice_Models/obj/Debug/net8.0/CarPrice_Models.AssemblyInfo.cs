f42dfa8af6929af4785b52c4405116b55fbe755d
KSteinke <kacpersteinkesb@gmail.com>
2024-02-23 20:19:48 +0100

    database mirgration added

--- CarPrice_Models/obj/Debug/net8.0/CarPrice_Models.AssemblyInfo.cs
+++ CarPrice_Models/obj/Debug/net8.0/CarPrice_Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarPrice_Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bcbe52fbf764d883c256fffca5345468574c744")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b6d35af20939c849c9a753359e4977571fce22")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarPrice_Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarPrice_Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
