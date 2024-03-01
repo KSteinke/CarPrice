6475b6ad350c07f613294169a1414def2774605b
KSteinke <kacpersteinkesb@gmail.com>
2024-03-01 17:54:12 +0100

    DemoScraperCreated

---
+++ CarPrice_Scraper/Entities/HarvestedData.cs
@@ -0,0 +1,11 @@
+namespace CarPrice_Scraper.Entities;
+
+public class HarvestedData
+{
+    string CarBrand;
+    string CarModel;
+    string Milage;
+    string EngineVolume;
+    string FuelType;
+    
+}
---
+++ WebScraperDemo/Entities/HarvestedData.cs
@@ -0,0 +1,12 @@
+using CarPrice_Models.Enums;
+namespace CarPrice_Scraper.Entities;
+
+public class HarvestedData
+{
+    string CarBrand;
+    string CarModel;
+    MilageGroupEnum Milage;
+    int EngineVolume;
+    FuelTypeEnum FuelType;
+    
+}
